4178316f1d58c7fa6a470c4c0047d67e28dad441
Lula Crackudo <lucasfonsecard@icloud.com>
2021-01-30 23:05:46 -0300

    add assets to global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from "styled-components";
+import githubBackground from "../assets/github-logo.svg";
 
 export default createGlobalStyle`
 	* {
@@ -8,10 +9,18 @@ export default createGlobalStyle`
 		box-sizing: border-box;
 	}
 	body {
-		background-color: #f0f0f5;
+		background: #f0f0f5 url(${githubBackground}) no-repeat 70% top;
 		-webkit-font-smoothing: antialiased;
 	}
 	body, input, button {
 		font: 16px Roboto, sans-serif;
+	}
+	#root {
+		max-width: 960px;
+		margin: 0 auto;
+		padding: 40px 20px;
+	}
+	button {
+		cursor: pointer;
 	}
 `;
