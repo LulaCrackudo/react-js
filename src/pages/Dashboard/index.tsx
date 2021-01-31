e1ffdc19c906f1b1b699c7f2a3df62956df6eebb
Lula Crackudo <lucasfonsecard@icloud.com>
2021-01-31 00:14:14 -0300

    add Repositories to Dashboard Component

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,11 @@
 // eslint-disable-next-line no-use-before-define
 import React from "react";
 
+import { FiChevronRight } from "react-icons/fi";
+
 import logo from "../../assets/app-logo.svg";
 
-import { Title, Form } from "./styles";
+import { Title, Form, Repositories } from "./styles";
 
 const Dashboard: React.FC = () => {
 	return (
@@ -15,6 +17,20 @@ const Dashboard: React.FC = () => {
 				<input placeholder="Digite o nome do repositório" />
 				<button type="submit">Pesquisar</button>
 			</Form>
+
+			<Repositories>
+				<a href="teste">
+					<img
+						src="https://avatars.githubusercontent.com/u/72674879?s=460&u=cfb2147bdbcfef4d353ca863db4a04f0ee47e4d4&v=4"
+						alt="Lula Crackudo"
+					/>
+					<div>
+						<strong>CS50</strong>
+						<p>Description</p>
+					</div>
+					<FiChevronRight size={20} />
+				</a>
+			</Repositories>
 		</>
 	);
 };
