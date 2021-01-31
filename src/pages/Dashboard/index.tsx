9356360ab22ee711d34d3c2f6b9678e8e42c5c0e
Lula Crackudo <lucasfonsecard@icloud.com>
2021-01-30 22:27:29 -0300

    fixed bug

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,10 @@
 // eslint-disable-next-line no-use-before-define
 import React from "react";
 
+import Title from "./styles";
+
 const Dashboard: React.FC = () => {
-	return <h1>Dashboard</h1>;
+	return <Title>Dashboard</Title>;
 };
 
 export default Dashboard;
